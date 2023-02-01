9c148e8a31412af6d43cbc43b352e5310ab0a2c8
my8za <emilie15@naver.com>
2023-02-01 23:00:23 +0900

    로그인 회원가입 기능 구현

---
+++ src/route/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// library
+import { Navigate } from 'react-router-dom';
+// components
+import Todo from '../page/Todo'
+
+
+// 로컬 스토리지에 토큰이 없는 상태로 /todo페이지에 접속한다면 /signin 경로로 리다이렉트
+// 로컬 스토리지에 토큰이 있는 상태로 /signin페이지에 접속한다면 /todo 경로로 리다이렉트
+export const CheckSignInPath = () => {
+  return localStorage.getItem('jwt-token') ? <Todo /> : <Navigate to='/signin'/>
+}
+
+// 로컬 스토리지에 토큰이 있는 상태로 /signup 페이지에 접속한다면 /todo 경로로 리다이렉트
+export const CheckSignUpPath = () => {
+  return localStorage.getItem('jwt-token') ? <Todo /> : <Navigate to='/signup'/>
+}
