287d77e2a0dccd8af88888c006d02d848e2c9dd9
my8za <emilie15@naver.com>
2023-02-10 01:32:34 +0900

    modify Home.js

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import '../style/home.scss';
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div className='home'>
       <h1 className='title'>Web Todo-List &#127936;</h1>
+      <button onClick={()=>{navigate('/signup')}}>Sign Up</button>
+      <button onClick={()=>{navigate('/signin')}}>Sign In</button>
+      <button onClick={()=>{navigate('/todo')}}>Todo</button>
     </div>
   )
 }
